61a6ca03f743fe7a4e4bd1c51fbf66322ba99d62
shamannaz <shamannazkonain5167@gmail.com>
2024-12-01 14:20:19 +0600

    faculty member updated

---
+++ pages/faculty_member.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+
+</head>
+<body>
+<?php include('../connections/header.php'); ?>
+<div class="Main">
+    <div class="Title">
+<h1>
+    FACULTY MEMBERS
+</h1>
+
+    </div>
+    <div class= "Faculty Info">
+      <img src="../image/campus.png" alt="" height="30px">
+      <strong>
+        Name
+      </strong>
+      <h2>
+        position
+      </h2>
+      <h3>
+        short brief
+      </h3>  
+
+    </div>
+
+</div>    
+
+</body>
+</html>
