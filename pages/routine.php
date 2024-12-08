3dac8d85f0fb0c02baca743473b557c80d093779
ArmanMahmud003 <armanmahmud003@gmail.com>
2024-12-09 02:41:54 +0600

    CSS Update

--- pages/routine.php
+++ pages/routine.php
@@ -6,25 +6,51 @@
     
    <body>
        <?php include('../connections/header.php'); ?>
-        
+      <div class="Routine_input">
 
        <form method="POST" action="final_routine.php">
             <br>
-            <strong>Course Code</strong><br>
-            <input type="text" name="cc1"><br>
-            <input type="text" name="cc2"><br>
-            <input type="text" name="cc3"><br>
-            <input type="text" name="cc4"><br>
-            <input type="text" name="cc5"><br>
-            <input type="text" name="cc6"><br>
-            <input type="text" name="cc7"><br>
-            <input type="submit" name="submit">
+           
+            <h1 style="color: white;">Course Code</h1><br>
+            <div class='background'>
+                <div class='box'>
+                <input type="text" name="cc1" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="text" name="cc2" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="text" name="cc3" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="text" name="cc4" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="text" name="cc5" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="text" name="cc6" class='wbox'><br>
+                </div>
+                <div class='box'>
+                    <input type="text" name="cc7" class='wbox'><br>
+                </div>
+                <div class='box'>
+                <input type="submit" name="submit">
+                </div>
+            
+           
+            
+            
+            
+            
+            
 
             
        </form>
+        </div>
+</div>
 
-
-
+       <?php include('../connections/footer.php'); ?>
 
    </body>
 
